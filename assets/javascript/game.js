8af9d13420ada598772641bce5cd539305147b6b
Jason Cartwright <jason.cartwright44@gmail.com>
2019-01-11 01:16:58 -0800

    logic done, except for reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,6 +24,7 @@ console.log("Gem 3 is : " + gem3);
 gem4 = Math.floor(Math.random() * 12)
 console.log("Gem 4 is : " + gem4); 
 
+$('#answer').text('Answer Number: ' + answerNumber);
 
 // Here we can restart the game
 function startGame() {
@@ -42,72 +43,88 @@ console.log("Gem 3 is : " + gem3);
 gem4 = Math.floor(Math.random() * 12)
 console.log("Gem 4 is : " + gem4); 
 count = 0;
+$('#answer').text('Answer Number: ' + answerNumber);
 }
+
 
 
 // if the user clicks on a gem, that number is added to the total (count)
 $('#gem1').on('click', function() {
 count = count + gem1;
+$('#current').text('Current Number: ' + count)
 console.log(count);
 if (count !==  0 && count === answerNumber) {
     wins++;
     console.log('Number of wins : ' + wins);
+    $('#wins').text('Wins: ' + wins)
     startGame();
 }
 
 else if (count > answerNumber) {
     losses++;
     console.log('Number of losses : ' + losses);
+    $('#losses').text('Losses: ' + losses)
     startGame();
 }
 });
 
 $('#gem2').on('click', function() {
 count = count + gem2;
+$('#current').text('Current Number: ' + count)
 console.log(count);
 if (count !==  0 && count === answerNumber) {
     wins++;
     console.log('Number of wins : ' + wins);
+    $('#wins').text('Wins: ' + wins)
     startGame();
 }
 
 else if (count > answerNumber) {
     losses++;
     console.log('Number of losses : ' + losses);
+    $('#losses').text('Losses: ' + losses)
     startGame();
 }
 });
 
 $('#gem3').on('click', function() {
 count = count + gem3;
+$('#current').text('Current Number: ' + count)
 console.log(count);
 if (count !==  0 && count === answerNumber) {
     wins++;
     console.log('Number of wins : ' + wins);
+    $('#wins').text('Wins: ' + wins)
+    
     startGame();
 }
 
 else if (count > answerNumber) {
     losses++;
     console.log('Number of losses : ' + losses);
+    $('#losses').text('Losses: ' + losses)
     startGame();
 }
 });
 
 $('#gem4').on('click', function() {
 count = count + gem4;
+$('#current').text('Current Number: ' + count)
 console.log(count);
 if (count !==  0 && count === answerNumber) {
     wins++;
     console.log('Number of wins : ' + wins);
+    $('#wins').text('Wins: ' + wins)
     startGame();
 }
 
 else if (count > answerNumber) {
     losses++;
     console.log('Number of losses : ' + losses);
+    $('#losses').text('Losses: ' + losses)
     startGame();
 }
 });
 
-
+// Have to still zero out the game after a win or loss
+// Have to still do the css, images, html :)
